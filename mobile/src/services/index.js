086d418d92ce40cfae0c082683320c7ff0300453
lucas azevedo da silva <lazevedo@gyramais.com>
2022-01-12 15:48:08 -0300

    adjust

--- mobile/src/services/index.js
+++ mobile/src/services/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://192.168.15.150:3030'
+    // baseURL:'http://192.168.15.150:3030'
+    baseURL:'http://192.168.1.108:3030'
 })
 
 export default api
